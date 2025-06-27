247340fcb9bf9a3a3497befdd3923304dae6d522
dongyeop-woo <dongyeopwoo1@gmail.com>
2025-06-27 13:43:35 +0900

    feat: UserMapper 클래스 추가(toEntity,toDto)

---
+++ src/main/java/com/social/login/mapper/UserMapper.java
@@ -0,0 +1,30 @@
+package com.social.login.mapper;
+
+import com.social.login.dto.UserDto;
+import com.social.login.entity.User;
+
+public class UserMapper {
+    public static User toEntity(UserDto dto) {
+        return User.builder()
+                .id(dto.getId())
+                .name(dto.getName())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .role(dto.getRole())
+                .socialType(dto.getSocialType())
+                .socialId(dto.getSocialId())
+                .build();
+    }
+
+    public static UserDto toDto(User entity) {
+        return UserDto.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .email(entity.getEmail())
+                .password(entity.getPassword())
+                .role(entity.getRole())
+                .socialType(entity.getSocialType())
+                .socialId(entity.getSocialId())
+                .build();
+    }
+} 
